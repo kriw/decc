2f0f4561b724bb4a3a0e6aa4b5248d852d2db828
kriw <kotarou777775@gmail.com>
2017-08-20 00:17:28 +0900

    add node

--- src/main.py
+++ src/main.py
@@ -1,5 +1,18 @@
 import re
 
+class Node:
+    def __init__(self, insn):
+        self.label = insn.ops[0]
+        initChild(insn)
+
+    def initChild(insn):
+        if insn.mnem in ['add', 'sub', 'imul', 'idiv', 'mul', 'div']:
+            self.childs = insn.ops
+        elif insn.mnem in ['mov', 'lea']:
+            self.childs = insn.ops[1:]
+        else:
+            print('unexepected mnemonic')
+
 class Insn:
     def __init__(self, insn):
         self.mnem = insn[0]
@@ -30,26 +43,42 @@ def isLiteral(s):
     return re.match(r'\-?[1-9][0-9]*', s) != None or \
             re.match(r'\-?0x[1-9][0-9]*', s) != None
 
-def toCode(insns):
-    if not 'local' in insns[0].ops[0]:
-        print(":Ha:")
+lblMap = {}
+def appendVersion(insn):
+    fmt = lambda x, v: x + '.' + str(v)
+    if len(insn.ops) > 1:
+        lbls = insn.ops[1:]
+        for i, l in enumerate(lbls):
+            if l in lblMap:
+                insn.ops[i+1] = fmt(l, lblMap[l])
+                
+    lbl = insn.ops[0]
+    if not lbl in lblMap:
+        lblMap[lbl] = -1
+    lblMap[lbl] += 1
+    insn.ops[0] = fmt(lbl, lblMap[lbl])
+    return insn
 
-    for insn in insns:
-        if insn.mnem in ['mov', 'lea']:
-            if 'local' in insn.ops[0]:
-                if isLiteral(insn.ops[1]):
-                    #end
+def isFuncMnem(mnem):
+    return mnem in ['call', 'push', 'pop', 'leave']
 
+def isAssign(mnem):
+    return mnem in ['mov', 'add', 'imul']
+
+def isLocal(operand):
+    return 'local' in operand
 
 def asm2LowIr(insns):
     for i, insn in enumerate(insns):
-        if insn.mnem in ['call', 'push', 'pop', 'leave']:
-            print("=====")
-            
+        if isFuncMnem(insn.mnem):
+            #TODO dealing with function call
+            insns[i] = Insn(['nop'])
         else:
             insn.ops = list(map(convRef, insn.ops))
-            if insn.mnem in ['mov', 'lea'] and 'local' in insn.ops[0]:
-                toCode(insns[:i+1][::-1])
+            if isAssign(insn.mnem)
+                # insn = appendVersion(insn)
+                insns[i] = insn
+    return insns
                 
 
 def parse(insns):
@@ -64,6 +93,15 @@ def parse(insns):
         parsedLines.append(Insn(c))
     return parsedLines
 
+def emit(insns):
+    
+    for insn in insns:
+        if isFulcMnem(insns.mnem):
+            print("func")
+        else:
+            if isAssign(insn.mnem) and isLocal(insn.ops[0]):
+
+
 def inputCode(fileName):
     f = open(fileName, 'r')
     insns = list(map(lambda x: x.strip(), f.readlines()))
@@ -72,7 +110,9 @@ def inputCode(fileName):
 def main(argv):
     insns = inputCode(argv[1])
     parsedLines = parse(insns)
-    asm2LowIr(parsedLines)
+    labeledAsm = asm2LowIr(parsedLines)
+    for l in labeledAsm:
+        print(str(l))
 
 
 if __name__ == '__main__':
