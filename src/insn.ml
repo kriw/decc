35c1e2695661b39ebcba629df48638f77941bb9a
kriw <kotarou777775@gmail.com>
2017-09-07 02:47:10 +0900

    WIP add update type insn

--- src/insn.ml
+++ src/insn.ml
@@ -1,13 +1,12 @@
 (* TODO *)type cond = unit
+let str_regs = ["eax"; "ebx"; "ecx"; "edx"; "edi"; "esi"]
 type reg = Eax | Ebx | Ecx | Edx | Edi | Esi | Else
 type addr = Addr of int32
 type label = Label of string * int32
 type mem = 
-  | Imm of addr
-  | Local of reg * int32
-  | Arg of reg * int32
-  | Direct of reg * int32   (*  reg + n  *)
-  | Indirect of reg * int32 (* [reg + n] *)
+  | Local of int32
+  | Arg of int32
+  | UnknownMem
 
 type expr =
   | Add of expr * expr
@@ -30,6 +29,7 @@ type insn =
   | Jmp of label * cond
   | Call of label
   | Ret
+  | Unknown
 
 let to_reg str =
   match str with
@@ -47,16 +47,48 @@ let gen_label =
   let _ = label_n := !label_n + 1 in
   Int32.of_int n
 let to_label addr = Label (addr, gen_label)
-let to_mem = 1
+
+(* FIXME *)
+let is_local str =
+  let pattern = Str.regexp "DWORD PTR \\[ebp-0x[0-9a-fA-F]\\]" in
+  Str.string_match pattern str 0
+
+(* FIXME *)
+let is_arg str =
+  let pattern = Str.regexp "DWORD PTR \\[ebp+0x[0-9a-fA-F]\\]" in
+  Str.string_match pattern str 0
+
+(* FIXME *)
+let to_local str = Int32.of_int 1
+let to_arg str = Int32.of_int 1
+
+let to_mem str =
+  match str with
+  | _str when is_local _str -> Local (to_local _str)
+  | _str when is_arg _str -> Arg (to_arg str)
+  | _ -> UnknownMem
+
+(* TODO *)
 let to_expr = 1
-let to_operand op = 1
+
+let is_imm op = 
+  let pattern = Str.regexp "0x[a-zA-Z0-9]+" in
+  Str.string_match pattern op 0
+
+let is_reg op = List.mem op str_regs
+let to_operand op =
+  match op with
+  | _op when is_imm _op -> Imm (Int32.of_string _op)
+  | _op when is_reg _op -> Reg (to_reg _op)
+  | _ -> Mem (to_mem op)
+
 let to_insn mnem ops =
   match mnem with
-  | "call" -> "call"
-  | "mov" -> "mov"
-  | "lea" -> "lea"
-  | "jmp" -> "jmp"
-  | "jle" -> "jmp less than equal"
-  | "ret" -> "ret"
-  | _ -> "unknown"
+  | "call" -> Call (to_label (List.nth ops 0))
+  | "mov" -> Mov (to_operand (List.nth ops 0), to_operand (List.nth ops 1))
+  | "lea" -> Lea (to_operand (List.nth ops 0), to_operand (List.nth ops 1))
+  | "jmp" -> Jmp (to_label (List.nth ops 0), ())
+  | "jle" -> Jmp (to_label (List.nth ops 0), ())
+  | "ret" -> Ret
+  | _ -> Unknown
 
