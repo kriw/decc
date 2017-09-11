0362a60b2c473dba148b7efadfde1f6e529452cd
kriw <kotarou777775@gmail.com>
2017-09-11 15:09:59 +0900

    impl add, mul

--- src/ast.ml
+++ src/ast.ml
@@ -1,3 +1,5 @@
+open Printf
+
 type ast =
   | Add of ast * ast
   | Sub of ast * ast
@@ -52,36 +54,74 @@ let eat_mov op1 op2 =
   | Asm.Reg reg -> ()
   | _ -> ()
 
-let get_state_reg line =
-  let operand = match line with
-    | Asm.Mov (op, _) -> op
-    | Asm.Lea (op, _) -> op
-    | Asm.Add (op, _) -> op
-    | Asm.Sub (op, _) -> op
-    | Asm.Mul (op, _) -> op
-    | Asm.Div op -> op
-    | Asm.Call op -> op
-    | _ -> Asm.Unknown in
-  let reg = match operand with
-    | Asm.Reg reg -> reg
-    | _ -> Asm.Else in
-  state_reg reg
+let first_op line = 
+  match line with
+  | Asm.Mov (op, _) -> op
+  | Asm.Lea (op, _) -> op
+  | Asm.Add (op, _) -> op
+  | Asm.Sub (op, _) -> op
+  | Asm.Mul (op, _) -> op
+  | Asm.Div op -> op
+  | Asm.Call op -> op
+  | _ -> Asm.Unknown
+
+let is_func_mnem line =
+  match line with
+  | Asm.Call _ -> true
+  | Asm.Ret  -> true
+  | _ -> false
+
+let is_local_from_op1 line =
+  let operand = first_op line in
+  match operand with
+  | Asm.Local str -> true
+  | _ -> false
+
+let is_reg_from_op1 line =
+  let operand = first_op line in
+  match operand with
+  | Asm.Reg reg -> true
+  | _ -> false
 
 let emit_ast line =
-  let reg = get_state_reg line in
+  let op1_is_reg = is_reg_from_op1 line in
   let ast = match line with
     | Asm.Mov (op1, op2) -> 
-      ref (Assign (!(state_ast op1), !(state_ast op2)))
+      let a = ref (Assign (!(state_ast op1), !(state_ast op2))) in
+      let _ = if op1_is_reg then
+          (state_ast op1) := !(state_ast op2)
+        else () in
+      a
     | Asm.Lea (op1, op2) -> 
-      ref (Assign (!(state_ast op1), Ref !(state_ast op2)))
+      let a = ref (Assign (!(state_ast op1), Ref !(state_ast op2))) in
+      let _ = if op1_is_reg then
+          (state_ast op1) := Ref !(state_ast op2)
+        else () in
+      a
     | Asm.Add (op1, op2) -> 
-      ref (Add (!(state_ast op1), !(state_ast op2)))
+      let a = ref (Add (!(state_ast op1), !(state_ast op2))) in
+      let _ = if op1_is_reg then
+          (state_ast op1) := !a
+        else () in
+      a
     | Asm.Sub (op1, op2) -> 
-      ref (Sub (!(state_ast op1), !(state_ast op2)))
+      let a = ref (Sub (!(state_ast op1), !(state_ast op2))) in
+      let _ = if op1_is_reg then
+          (state_ast op1) := !a
+        else () in
+      a
     | Asm.Mul (op1, op2) ->
-      ref (Mul (!(state_ast op1), !(state_ast op2)))
+      let a = ref (Mul (!(state_ast op1), !(state_ast op2))) in
+      let _ = if op1_is_reg then
+          (state_ast op1) := !a
+        else ()in
+      a
     | Asm.Div op ->
-      ref (Div (!State.eax, !(state_ast op)))
+      let a = ref (Div (!State.eax, !(state_ast op))) in
+      let _ = if op1_is_reg then
+          (state_ast op) := !a
+        else () in
+      a
     | Asm.Cmp (op1, op2) ->
       ref (Cond (!(state_ast op1), !(state_ast op2)))
     | Asm.Jmp op ->
@@ -91,15 +131,31 @@ let emit_ast line =
     | Asm.Ret ->
       ref (Ret !State.eax)
     | _ -> ref Emp in
-  let _ = match !reg with
-    | Emp -> ()
-    | _ -> reg := !ast in
   ast
 
-(* TODO *)
-let rec to_ast asms =
-  match asms with
-  | [] -> ref Emp
-  | x::xs -> 
-    let ast = emit_ast x in
-    ast
+let rec to_string ast =
+  match ast with
+  | Add (ast1, ast2) -> sprintf "(%s + %s)" (to_string ast1) (to_string ast2)
+  | Sub (ast1, ast2) -> sprintf "(%s - %s)" (to_string ast1) (to_string ast2)
+  | Mul (ast1, ast2) -> sprintf "(%s * %s)" (to_string ast1) (to_string ast2)
+  | Div (ast1, ast2) -> sprintf "(%s / %s)" (to_string ast1) (to_string ast2)
+  | Assign (ast1, ast2) -> sprintf "%s = %s" (to_string ast1) (to_string ast2)
+  | Jmp ast -> "" (* TODO *)
+  | Cond (ast1, ast2) -> "" (* TODO *)
+  | Call ast -> "" (* TODO *)
+  | Ret ast -> sprintf "return %s" (to_string ast)
+  | Ref ast -> sprintf "&(%s)"  (to_string ast)
+  | Value op -> Asm.op_to_string op
+  | Emp -> "emp"
+
+let to_ast asms = 
+  let rec _to_ast asms ast =
+    match asms with
+    | [] -> List.rev ast
+    | x::xs -> 
+      let _ast = emit_ast x in
+      match x with
+      | x when (is_local_from_op1 x) || (is_func_mnem x) -> 
+        _to_ast xs ((!_ast)::ast)
+      | _ -> _to_ast xs ast in
+  _to_ast asms []
--- src/main.ml
+++ src/main.ml
@@ -7,5 +7,7 @@ let _ =
       let insns, addrs = parse v in
       let mnems, ops = insns in
       let asms = List.map (fun (x, y) -> Asm.to_asm x y) (List.combine mnems ops) in
-      print_endline k; List.iter (fun asm -> print_endline (Asm.to_string asm)) asms
+      let asts = Ast.to_ast asms in
+      List.iter (fun x -> print_endline (Ast.to_string x)) asts
+      (* print_endline k; List.iter (fun asm -> print_endline (Asm.to_string asm)) asms *)
     ) codes
